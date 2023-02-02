a51998b776ba8ec886505a051ed480eaca3b72d1
Shamim Ahmed <shamim.cse.2024@gmail.com>
2023-02-02 13:12:35 +0600

    Add files via upload

---
+++ Variable&Typeasting.py
@@ -0,0 +1,22 @@
+
+name= "Shamim Ahmed"
+phone = "01866308016"
+cgpa= 3.90
+
+print("My name is "+name+" and phone number "+phone+" and cgpa",cgpa)
+
+# now lets try with users input
+
+name=input("Enter the name ")
+phone =input("Put the number ")
+print("My name is "+name+"\n My phone number is "+phone)
+
+#TypeCasting
+
+name =input("Enter your name ")
+phone=int(input("Enter your Phone number "))
+num1=int(input("number1 "))
+num2= int(input("number2 "))
+
+result = num1+num2
+print("the sum is ",result)
