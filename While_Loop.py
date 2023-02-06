e95dba7f8632ab185d55b8e2d952668e14e94060
Shamim Ahmed <shamim.cse.2024@gmail.com>
2023-02-06 15:00:48 +0600

    Add files via upload

---
+++ While_Loop.py
@@ -0,0 +1,24 @@
+
+
+i=1
+while i<11:
+    print(i)
+    i=i+1
+
+print("USe brake ")
+
+i=1
+while i<11:
+    print(i)
+    if i==6:
+        break
+    i=i+1
+print("USe  continue ")
+i=1
+while i<11:
+    print(i)
+    if i==9:
+        continue
+    i=i+1
+
+print("End the programe ")
