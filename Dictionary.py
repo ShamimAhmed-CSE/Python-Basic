72403d3a4843580103a40c3b80cbfdc692dafdb9
Shamim Ahmed <shamim.cse.2024@gmail.com>
2023-03-04 16:26:25 +0600

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+
+my_info={
+    "Name" : "Shamim Ahmed",
+    "ID" : 20902067,
+    "Section " : "DB",
+    "Course " : "AI"
+}
+print(my_info)
+print(my_info["Course "])
+
+#for changing a value in dictionary
+
+my_info["Course "]="OS"
+print(my_info)
+
+#for adding new key in dictionary
+my_info["Year"]=2023
+print(my_info)
+
